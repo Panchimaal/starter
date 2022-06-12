a886db30ef5cfd49fa175565e298491b0a85d173
Panchimaal <panchimaal@hotmail.com>
2022-06-11 19:14:30 -0500

    Initial commit

---
+++ db/connect.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+
+
+
+
+const connectDB = (url) => {
+return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    //UseFindAndModify: false,
+    useUnifiedTopology: true, 
+})
+}
+
+module.exports = connectDB
+
+
